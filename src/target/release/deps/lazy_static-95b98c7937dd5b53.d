65fb5348d28ec25800dc6c0dcca75690e1d9f014
Nicolas Elias <nicolaselias@pm.me>
2025-06-16 17:24:18 -0300

    wip: rm arquivos de compilacao

--- src/target/release/deps/lazy_static-95b98c7937dd5b53.d
+++
@@ -1,8 +0,0 @@
-/home/ncls/ufsc/compiladores/main/src/target/release/deps/liblazy_static-95b98c7937dd5b53.rmeta: /home/ncls/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/lib.rs /home/ncls/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/inline_lazy.rs
-
-/home/ncls/ufsc/compiladores/main/src/target/release/deps/liblazy_static-95b98c7937dd5b53.rlib: /home/ncls/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/lib.rs /home/ncls/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/inline_lazy.rs
-
-/home/ncls/ufsc/compiladores/main/src/target/release/deps/lazy_static-95b98c7937dd5b53.d: /home/ncls/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/lib.rs /home/ncls/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/inline_lazy.rs
-
-/home/ncls/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/lib.rs:
-/home/ncls/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/inline_lazy.rs:
--- target/release/deps/compiler-13d27f8562241b86.d
+++
@@ -1,5 +0,0 @@
-/home/ncls/ufsc/compiladores/main/target/release/deps/compiler-13d27f8562241b86: src/main.rs
-
-/home/ncls/ufsc/compiladores/main/target/release/deps/compiler-13d27f8562241b86.d: src/main.rs
-
-src/main.rs:
